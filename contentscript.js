d7c1bb02d4849a6d3457d185f167874c056b0ef6
Edgar Ruiz <edgarr@eden.newyork.brandwatch.net>
2019-02-09 22:31:27 -0500

    Fix class name

--- contentscript.js
+++ contentscript.js
@@ -9,12 +9,12 @@ function getData(){
 	email = document.getElementsByClassName('email')[0].innerHTML;
 	console.log('User: ' + email);
 	project = document.getElementsByClassName('project-text textellipsis')[0].innerHTML;
-	dashboard = document.getElementsByClassName('js-name')[0].innerHTML;
+	dashboard = document.getElementsByClassName('textellipsis dib eightcolmax')[0].innerText;
 	email = 'User: ' + email;
 	project = ('Project: ' + project + ' - ' +  location.href.substr(location.href.indexOf('project')+ 8,10));
 
 	dashboard = ('Dashboard: ' + dashboard + ' - ' + location.href);
-	tab = document.getElementsByClassName('dragHandle horizontal-navigation--item navbuttonshade dib current active')[0].innerText;
+	tab = document.getElementsByClassName('horizontal-navigation--item navbuttonshade dib current active')[0].innerText;
 	tab = 'Tab: ' + tab;
 
 	info = email + '\n' + project + '\n' + dashboard + '\n' + tab;
